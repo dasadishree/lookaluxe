4c21ed2eeaf98699cf2325f0d0d2f0ae9354ab5f
Adishree Das <dasadishree@gmail.com>
2025-06-06 23:23:49 -0400

    start

--- Lookaluxe/ContentView.swift
+++ Lookaluxe/ContentView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "diamond")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(Color(red: 191/255, green: 121/255, blue: 71/255))
+                .padding(10)
+                .background(
+                    Circle().fill(Color.brown.opacity(0.2))
+                )
+            Text("Welcome to")
+                .font(.system(size: 20, design:.serif))
+                .fontWeight(.heavy)
+            Text("Lookaluxe")
+                .font(.system(size: 50, design:.serif))
+                .fontWeight(.heavy)
+                .foregroundColor(.brown)
+                .italic()
+            
+            HStack {
+                Button("Explore", action: {print("works")})
+                    .background(Color(red: 191/255, green: 121/255, blue: 71/255))
+                    .foregroundColor(.white)
+                    .buttonStyle(.bordered)
+                
+                Button("Allow Access to Photos", action: {print("allows works")})
+                    .buttonStyle(.bordered)
+                    .background(Color(red: 191/255, green: 121/255, blue: 71/255))
+                    .foregroundColor(.white)
+            }
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight:.infinity)
+        .background(Color.brown.opacity(0.2))
     }
 }
 
--- Lookaluxe/LookaluxeApp.swift
+++ Lookaluxe/LookaluxeApp.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 @main
+
 struct LookaluxeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .accentColor(Color(red: 163/255, green: 107/255, blue: 67/255 ))
         }
     }
 }
