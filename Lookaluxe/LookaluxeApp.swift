aa62799b18f7ebcf76606b4dbef5729ee3eadd4e
Adishree Das <dasadishree@gmail.com>
2025-06-06 21:23:12 -0400

    Initial Commit

---
+++ Lookaluxe/LookaluxeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookaluxeApp.swift
+//  Lookaluxe
+//
+//  Created by Adishree Das on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LookaluxeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
